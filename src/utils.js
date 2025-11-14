c8807e367db16d89f25faef77bc9b83ae3c72efe
baldur-logi <baldur.logi@gmail.com>
2025-11-14 00:07:31 +0000

    laga test fail

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export function createImageElement(url) {
   const img = document.createElement("img");
   img.src = url;
-  img.alt = "Handahófskennd dýramynd";
+  img.alt = "Mynd af hundi eða ketti, valin af handahófi";
   img.setAttribute("aria-label", "Handahófskennd mynd af kötti eða hundi");
   return img;
 }
